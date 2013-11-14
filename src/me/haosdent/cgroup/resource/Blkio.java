c6698f3d5ad3f89f2a903e8740890c65290392f6
haosdent <haosdent@gmail.com>
2013-11-15 02:50:02 +0800

    Add method defines to Blkio class.

--- src/me/haosdent/cgroup/resource/Blkio.java
+++ src/me/haosdent/cgroup/resource/Blkio.java
@@ -1,4 +1,13 @@
 package me.haosdent.cgroup.resource;
 
 public class Blkio {
+  public static void setReadBpsThrottle() {};
+
+  public static void setWriteBpsThrottle() {};
+
+  public static void setReadIopsThrottle() {};
+
+  public static void setWriteIopsThrottle() {};
+
+  public static void resetStats() {};
 }
