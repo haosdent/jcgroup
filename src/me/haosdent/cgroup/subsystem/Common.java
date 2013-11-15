c02bccbff8c1d886f80248f0aed81439b0689e6e
haosdent <haosdent@gmail.com>
2013-11-15 17:04:03 +0800

    Add Common.java.

---
+++ src/me/haosdent/cgroup/subsystem/Common.java
@@ -0,0 +1,14 @@
+package me.haosdent.cgroup.subsystem;
+
+public class Common {
+
+  public static void addThreadId() {}
+
+  public static void addThreadGroupId() {}
+
+  public static void setEventControl() {}
+
+  public static void setNotifyOnRelease() {}
+
+  public static void setReleaseAgent() {}
+}
