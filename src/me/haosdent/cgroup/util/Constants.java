d8e42a5d51de3d4fcff386011511e22ca840336e
haosdent <haosdent@gmail.com>
2013-11-17 15:56:53 +0800

    Add SHELL_CONSTATNs to Constatns.java

--- src/me/haosdent/cgroup/util/Constants.java
+++ src/me/haosdent/cgroup/util/Constants.java
@@ -1,4 +1,15 @@
 package me.haosdent.cgroup.util;
 
 public class Constants {
+
+  public static final String SHELL_MOUNT = "mount -t cgroup -o %s %s %s";
+  public static final String SHELL_UMOUNT = "umount %s";
+
+  public static final String SHELL_CG_CLEAR = "cgclear";
+  public static final String SHELL_CG_CREATE = "cgcreate -g %s:%s";
+  public static final String SHELL_CG_DELETE = "cgdelete %s:%s";
+  public static final String SHELL_CG_SET = "cgset -r %s=%s %s";
+  public static final String SHELL_CG_CLASSIFY = "cgclassify -g %s:%s %s";
+  public static final String SHELL_CG_EXEC = "cgexec -g %s:%s %s %s";
+
 }
