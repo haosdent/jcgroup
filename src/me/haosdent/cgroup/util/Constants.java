8ea574c527fd2e444f4196ebb38ee8810db83dbb
haosdent <haosdent@gmail.com>
2013-11-17 18:04:40 +0800

    Add SUBSYS constants.

--- src/me/haosdent/cgroup/util/Constants.java
+++ src/me/haosdent/cgroup/util/Constants.java
@@ -12,4 +12,13 @@ public class Constants {
   public static final String SHELL_CG_CLASSIFY = "cgclassify -g %s:%s %s";
   public static final String SHELL_CG_EXEC = "cgexec -g %s:%s %s %s";
 
+  public static final int SUBSYS_BLKIO = 0;
+  public static final int SUBSYS_CPU = 1;
+  public static final int SUBSYS_CPUACCT = 2;
+  public static final int SUBSYS_CPUSET = 3;
+  public static final int SUBSYS_DEVICE = 4;
+  public static final int SUBSYS_FREEZER = 5;
+  public static final int SUBSYS_MEMORY = 6;
+  public static final int SUBSYS_NET_CLS = 7;
+  public static final int SUBSYS_NET_PRIO = 8;
 }
