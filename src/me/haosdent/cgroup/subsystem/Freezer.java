2f8c6d153f9cb3cd63e4ce041fd3420bc7ee4937
haosdent <haosdent@gmail.com>
2013-12-07 00:39:40 +0800

    Implement methods in Freezer.java

--- src/me/haosdent/cgroup/subsystem/Freezer.java
+++ src/me/haosdent/cgroup/subsystem/Freezer.java
@@ -3,15 +3,27 @@ package me.haosdent.cgroup.subsystem;
 import me.haosdent.cgroup.manage.Group;
 import me.haosdent.cgroup.util.Constants;
 
+import java.io.IOException;
+
 public class Freezer extends Common {
 
-  public static final int subsystem = Constants.SUBSYS_FREEZER;
+  public static final int SUBSYS = Constants.SUBSYS_FREEZER;
+  public static final String PROP_FREEZER_STATE = "freezer.state";
+
+  public static final String STATE_FROZEN = "FROZEN";
+  public static final String STATE_FREEZING = "FREEZING";
+  public static final String STATE_THAWED = "THAWED";
 
   public Freezer(Group group) {
     super(group);
   }
 
-  public void setState() {}
+  public void setState(String state) throws IOException {
+    shell.cgset(group.getName(), PROP_FREEZER_STATE, state);
+  }
 
-  public void getState() {}
+  public String getState() {
+    String state = shell.cgget(group.getName(), PROP_FREEZER_STATE);
+    return state;
+  }
 }
