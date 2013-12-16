81f10a80ba03edad0a9f7a58028c06efff29be35
haosdent <haosdent@gmail.com>
2013-12-17 01:53:40 +0800

    Implement methods in TestThreads.

--- src/test/java/me/haosdent/cgroup/util/TestThreads.java
+++ src/test/java/me/haosdent/cgroup/util/TestThreads.java
@@ -3,15 +3,25 @@ package me.haosdent.cgroup.util;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.*;
 
 public class TestThreads {
 
-  @Before
-  public void setUp() {}
-
-  @After
-  public void tearDown() {}
+  private static final Logger LOG = LoggerFactory.getLogger(TestThreads.class);
 
   @Test
-  public void testGetThreadId() {}
+  public void testGetThreadId() {
+    Thread thread = new Thread() {
+      @Override
+      public void run() {
+        int id = Threads.getThreadId();
+        LOG.info("Thread Id: " + id);
+        assertTrue(id != 0);
+      }
+    };
+    thread.start();
+  }
 }
