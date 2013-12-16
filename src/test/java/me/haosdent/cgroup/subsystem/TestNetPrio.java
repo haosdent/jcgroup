083b3bb63c28220f3fe9908e6f59b6b64d7004a7
haosdent <haosdent@gmail.com>
2013-12-16 21:56:58 +0800

    Add net_prio subsystem test class.

---
+++ src/test/java/me/haosdent/cgroup/subsystem/TestNetPrio.java
@@ -0,0 +1,23 @@
+package me.haosdent.cgroup.subsystem;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestNetPrio {
+
+  @Before
+  public void setUp() {}
+
+  @After
+  public void tearDown() {}
+
+  @Test
+  public void testGetPrioId() {}
+
+  @Test
+  public void testGetIfPrioMap() {}
+
+  @Test
+  public void testAddIfPrioMap() {}
+}
