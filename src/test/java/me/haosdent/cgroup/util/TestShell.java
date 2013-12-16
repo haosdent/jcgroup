d877c7749fba6597a997c7f9318d1659e241e231
haosdent <haosdent@gmail.com>
2013-12-16 22:36:09 +0800

    Add shell util test class.

---
+++ src/test/java/me/haosdent/cgroup/util/TestShell.java
@@ -0,0 +1,23 @@
+package me.haosdent.cgroup.util;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestShell {
+
+  @Before
+  public void setUp() {}
+
+  @After
+  public void tearDown() {}
+
+  @Test
+  public void testExec() {}
+
+  @Test
+  public void testMount() {}
+
+  @Test
+  public void testUmount() {}
+}
