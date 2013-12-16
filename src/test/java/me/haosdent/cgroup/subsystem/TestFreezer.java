bf8c60903e91767cecdb306c9f5f1ec2498556e3
haosdent <haosdent@gmail.com>
2013-12-16 21:31:19 +0800

    Add freezer subsystem test class.

---
+++ src/test/java/me/haosdent/cgroup/subsystem/TestFreezer.java
@@ -0,0 +1,20 @@
+package me.haosdent.cgroup.subsystem;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestFreezer {
+
+  @Before
+  public void setUp() {}
+
+  @After
+  public void tearDown() {}
+
+  @Test
+  public void testSetState() {}
+
+  @Test
+  public void testGetState() {}
+}
