c6acca92b38e577e90d2f766e81e95f0853e57bb
haosdent <haosdent@gmail.com>
2013-12-16 21:12:25 +0800

    Add devices subsystem test class.

---
+++ src/test/java/me/haosdent/cgroup/subsystem/TestDevices.java
@@ -0,0 +1,23 @@
+package me.haosdent.cgroup.subsystem;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestDevices {
+
+  @Before
+  public void setUp() {}
+
+  @After
+  public void tearDown() {}
+
+  @Test
+  public void testSetAllow() {}
+
+  @Test
+  public void testSetDeny() {}
+
+  @Test
+  public void testGetList() {}
+}
