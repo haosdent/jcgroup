9ec52a54d1dbffbaef7a3e40e5600924a7c4971f
haosdent <haosdent@gmail.com>
2013-12-16 21:40:42 +0800

    Add memory subsystem test class.

---
+++ src/test/java/me/haosdent/cgroup/subsystem/TestMemory.java
@@ -0,0 +1,53 @@
+package me.haosdent.cgroup.subsystem;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestMemory {
+
+  @Before
+  public void setUp() {}
+
+  @After
+  public void tearDown() {}
+
+  @Test
+  public void testGetStat() {}
+
+  @Test
+  public void testGetPhysicalUsage() {}
+
+  @Test
+  public void testGetWithSwapUsage() {}
+
+  @Test
+  public void testGetMaxPhysicalUsage() {}
+
+  @Test
+  public void testGetMaxWithSwapUsage() {}
+
+  @Test
+  public void testSetPhysicalUsageLimit() {}
+
+  @Test
+  public void testSetWithSwapUsageLimit() {}
+
+  @Test
+  public void testGetPhysicalFailCount() {}
+
+  @Test
+  public void testGetWithSwapFailCount() {}
+
+  @Test
+  public void testClearForceEmpty() {}
+
+  @Test
+  public void testSetSwappiness() {}
+
+  @Test
+  public void testSetUseHierarchy() {}
+
+  @Test
+  public void testSetOomControl() {}
+}
