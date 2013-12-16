79b0b143636f895b30341c1f14cb2008711c378a
haosdent <haosdent@gmail.com>
2013-12-16 22:02:57 +0800

    Add admin manage test class.

---
+++ src/test/java/me/haosdent/cgroup/manage/TestAdmin.java
@@ -0,0 +1,20 @@
+package me.haosdent.cgroup.manage;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestAdmin {
+
+  @Before
+  public void setUp() {}
+
+  @After
+  public void tearDown() {}
+
+  @Test
+  public void testUmount() {}
+
+  @Test
+  public void testCreateGroup() {}
+}
