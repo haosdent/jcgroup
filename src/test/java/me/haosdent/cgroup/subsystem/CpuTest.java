7bb1cb4651a70464e4b79dba55b78949a338b965
haosdent <haosdent@gmail.com>
2013-12-23 07:18:45 +0800

    Start to implement testSetShares method in CpuTest.java.

--- src/test/java/me/haosdent/cgroup/subsystem/CpuTest.java
+++ src/test/java/me/haosdent/cgroup/subsystem/CpuTest.java
@@ -1,8 +1,19 @@
 package me.haosdent.cgroup.subsystem;
 
+import me.haosdent.cgroup.manage.Admin;
+import me.haosdent.cgroup.manage.Group;
+import me.haosdent.cgroup.util.Constants;
 import org.junit.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
 
 public class CpuTest {
+
+  private static final Logger LOG = LoggerFactory.getLogger(CpuTest.class);
 
   @BeforeClass
   public void setUpClass() {}
@@ -11,7 +22,17 @@ public class CpuTest {
   public void tearDownClass() {}
 
   @Test
-  public void testSetShares() {}
+  public void testSetShares() {
+    try {
+      Admin admin = new Admin(Constants.SUBSYS_CPU);
+      Group one = admin.createGroup("one", Constants.SUBSYS_CPU);
+      Group two = admin.createGroup("two", Constants.SUBSYS_CPU);
+      //TODO
+    } catch (IOException e) {
+      LOG.error("Create Admin Failed.", e);
+      assertTrue(false);
+    }
+  }
 
   @Test
   public void testSetCfsPeriodTime() {}
