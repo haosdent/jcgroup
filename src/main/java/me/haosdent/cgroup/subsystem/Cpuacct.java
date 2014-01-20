9983071773b6454aa458a1643649183d62bbf637
haosdent <haosdent@gmail.com>
2014-01-20 16:07:19 +0800

    Implement testGetUsage,testResetUsage,testGetStat,testGetPerUsage in CpuacctTest.java

--- src/main/java/me/haosdent/cgroup/subsystem/Cpuacct.java
+++ src/main/java/me/haosdent/cgroup/subsystem/Cpuacct.java
@@ -21,9 +21,20 @@ public class Cpuacct extends Common {
     public final long systemTime;
 
     public Stat(String statStr) {
-      //TODO
-      userTime = 1;
-      systemTime = 1;
+      String[] splits = statStr.split("\n");
+      this.userTime = Integer.parseInt(splits[0].split(" ")[1]);
+      this.systemTime = Integer.parseInt(splits[1].split(" ")[1]);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      Stat excepted = (Stat) obj;
+      boolean result = false;
+      if (this.userTime == excepted.userTime
+              && this.systemTime == excepted.systemTime) {
+        result = true;
+      }
+      return result;
     }
   }
 
@@ -42,9 +53,12 @@ public class Cpuacct extends Common {
     return stat;
   }
 
-  public long getPerUsage() throws IOException {
-    //TODO
-    String result = shell.cgget(group.getName(), PROP_CPUACCT_USAGE_PERCPU);
-    return Long.parseLong(result);
+  public long[] getPerUsage() throws IOException {
+    String[] results = shell.cgget(group.getName(), PROP_CPUACCT_USAGE_PERCPU).split(" ");
+    long[] usages = new long[results.length];
+    for (int i = 0, l = results.length; i < l; i++) {
+      usages[i] = Long.parseLong(results[i]);
+    }
+    return usages;
   }
 }
--- src/test/java/me/haosdent/cgroup/subsystem/CpuacctTest.java
+++ src/test/java/me/haosdent/cgroup/subsystem/CpuacctTest.java
@@ -1,10 +1,43 @@
 package me.haosdent.cgroup.subsystem;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import me.haosdent.cgroup.manage.Admin;
+import me.haosdent.cgroup.manage.Group;
+import me.haosdent.cgroup.util.Constants;
+import org.junit.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
 
 public class CpuacctTest {
+
+  private static final Logger LOG = LoggerFactory.getLogger(CpuacctTest.class);
+  private static Admin admin;
+  private static Group one;
+  private static Group two;
+
+  @BeforeClass
+  public static void setUpClass() {
+    try {
+      Admin admin = new Admin(Constants.SUBSYS_CPUACCT);
+      Group one = admin.createGroup("one", Constants.SUBSYS_CPUACCT);
+      Group two = admin.createGroup("two", Constants.SUBSYS_CPUACCT);
+    } catch (IOException e) {
+      LOG.error("Create cgroup Failed.", e);
+      assertTrue(false);
+    }
+  }
+
+  @AfterClass
+  public static void tearDownClass() {
+    try {
+      admin.umount();
+    } catch (IOException e) {
+      LOG.error("Umount cgroup failed.", e);
+    }
+  }
 
   @Before
   public void setUp() {}
@@ -13,14 +46,51 @@ public class CpuacctTest {
   public void tearDown() {}
 
   @Test
-  public void testGetUsage() {}
+  public void testGetUsage() {
+    try {
+      long usage = one.getCpuacct().getUsage();
+      assertTrue(usage >= 0);
+    } catch (IOException e) {
+      LOG.error("Get usage failed.", e);
+      assertTrue(false);
+    }
+  }
 
   @Test
-  public void testResetUsage() {}
+  public void testResetUsage() {
+    try {
+      one.getCpuacct().resetUsage();
+      long usage = one.getCpuacct().getUsage();
+      assertEquals(usage, 0);
+    } catch (IOException e) {
+      LOG.error("Get usage failed.", e);
+      assertTrue(false);
+    }
+  }
 
   @Test
-  public void testGetStat() {}
+  public void testGetStat() {
+    try {
+      Cpuacct.Stat stat = one.getCpuacct().getStat();
+      assertTrue(stat.userTime >= 0);
+      assertTrue(stat.systemTime >= 0);
+    } catch (IOException e) {
+      LOG.error("Get stat failed.", e);
+      assertTrue(false);
+    }
+  }
 
   @Test
-  public void testGetPerUsage() {}
+  public void testGetPerUsage() {
+    try {
+      long[] usages = one.getCpuacct().getPerUsage();
+      assertTrue(usages.length > 0);
+      for (long usage: usages) {
+        assertTrue(usage > 0);
+      }
+    } catch (IOException e) {
+      LOG.error("Get usage_percpu failed.", e);
+      assertTrue(false);
+    }
+  }
 }
